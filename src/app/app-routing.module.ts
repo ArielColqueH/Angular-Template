b5256bb12451dcd4598297a7a7ffa0298bcb2735
ArielColqueH <arielcolque444@gmail.com>
2020-10-26 18:59:06 -0400

    login and dialogs part added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-
-
-const routes: Routes = [];
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { HomeComponent } from "./modules/home/home.component";
+import { LoginComponent } from "./modules/home/pages/login/login.component";
+const routes: Routes = [
+  {
+    path: "",
+    component: LoginComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './modules/home/home.component';
 import { HeaderComponent } from './layout/header/header.component';
 import { FooterComponent } from './layout/footer/footer.component';
+import { DialogsComponent } from './modules/dialogs/dialogs.component';
+import { LoginComponent } from './modules/home/pages/login/login.component';
 
 
 @NgModule({
@@ -14,6 +16,8 @@ import { FooterComponent } from './layout/footer/footer.component';
     HomeComponent,
     HeaderComponent,
     FooterComponent,
+    DialogsComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
